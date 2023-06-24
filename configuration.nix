560844cd4945c7575c59b22bd9a9df71dd4c7203
Marc Partensky <marc@marcpartensky.com>
2023-06-24 15:31:02 +0200

    kubernetes zfs support

--- configuration.nix
+++ configuration.nix
@@ -51,13 +51,38 @@
     settings = { PasswordAuthentication = lib.mkDefault true; };
   };
 
-  # kubernetes
+  # kubernetes https://nixos.wiki/wiki/K3s
   networking.firewall.allowedTCPPorts = [ 6443 ];
   services.k3s = {
     enable = true;
     role = "server";
     extraFlags = toString [];
   };
+
+  # kubernetes zfs support
+  virtualisation.containerd = {
+    enable = true;
+    settings =
+      let
+        fullCNIPlugins = pkgs.buildEnv {
+          name = "full-cni";
+          paths = with pkgs;[
+            cni-plugins
+            cni-plugin-flannel
+          ];
+        };
+      in {
+        plugins."io.containerd.grpc.v1.cri".cni = {
+          bin_dir = "${fullCNIPlugins}/bin";
+          conf_dir = "/var/lib/rancher/k3s/agent/etc/cni/net.d/";
+        };
+      };
+  };
+  # TODO describe how to enable zfs snapshotter in containerd
+  services.k3s.extraFlags = toString [
+    "--container-runtime-endpoint unix:///run/containerd/containerd.sock"
+  ];
+
 
   boot.zfs.forceImportRoot = lib.mkDefault false;
 

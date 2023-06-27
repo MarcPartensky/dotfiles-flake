7a1de01ef3553c44a4570d97d3be1d8ba73c53ed
Marc Partensky <marc.partensky@gmail.com>
2023-06-27 09:45:31 +0000

    exampleHost -> nixos

--- install.sh
+++ install.sh
@@ -164,7 +164,7 @@ log Customizing configuration to your hardware
 for i in ${DISK}; do
   sed -i \
   "s|/dev/disk/by-id/|${i%/*}/|" \
-  "${MNT}"/etc/nixos/hosts/exampleHost/default.nix
+  "${MNT}"/etc/nixos/hosts/nixos/default.nix
   break
 done
 
@@ -174,10 +174,10 @@ for i in ${DISK}; do
 done
 
 sed -i "s|\"bootDevices_placeholder\"|${diskNames}|g" \
-  "${MNT}"/etc/nixos/hosts/exampleHost/default.nix
+  "${MNT}"/etc/nixos/hosts/nixos/default.nix
 
 sed -i "s|\"abcd1234\"|\"$(head -c4 /dev/urandom | od -A none -t x4| sed 's| ||g' || true)\"|g" \
-  "${MNT}"/etc/nixos/hosts/exampleHost/default.nix
+  "${MNT}"/etc/nixos/hosts/nixos/default.nix
 
 sed -i "s|\"x86_64-linux\"|\"$(uname -m || true)-linux\"|g" \
   "${MNT}"/etc/nixos/flake.nix
@@ -192,7 +192,7 @@ echo 'print STDOUT $initrdAvailableKernelModules' >> ./nixos-generate-config
 kernelModules="$(./nixos-generate-config --show-hardware-config --no-filesystems | tail -n1 || true)"
 
 sed -i "s|\"kernelModules_placeholder\"|${kernelModules}|g" \
-  "${MNT}"/etc/nixos/hosts/exampleHost/default.nix
+  "${MNT}"/etc/nixos/hosts/nixos/default.nix
 
 log Setting root password
 # ---

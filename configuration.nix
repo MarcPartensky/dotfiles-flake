a114e9ddb93f5aa3a90f1bcb2438cae7c33dbb95
Marc Partensky <marc@marcpartensky.com>
2023-07-07 04:05:11 +0200

    kubernetes pls

--- configuration.nix
+++ configuration.nix
@@ -91,6 +91,41 @@
       };
   };
 
+  # kubernetes support
+  let
+    kubeMasterIP = "10.1.1.2";
+    kubeMasterHostname = "api.kube";
+    kubeMasterAPIServerPort = 6443;
+  in
+  {
+    # resolve master hostname
+    networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
+  
+    # packages for administration tasks
+    environment.systemPackages = with pkgs; [
+      kompose
+      kubectl
+      kubernetes
+    ];
+  
+    services.kubernetes = {
+      roles = ["master" "node"];
+      masterAddress = kubeMasterHostname;
+      apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
+      easyCerts = true;
+      apiserver = {
+        securePort = kubeMasterAPIServerPort;
+        advertiseAddress = kubeMasterIP;
+      };
+  
+      # use coredns
+      addons.dns.enable = true;
+  
+      # needed if you use swap
+      kubelet.extraOpts = "--fail-swap-on=false";
+    };
+  }
+
   boot.zfs.forceImportRoot = lib.mkDefault false;
 
   nix.settings.experimental-features = lib.mkDefault [ "nix-command" "flakes" ];

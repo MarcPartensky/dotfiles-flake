abcb04846a44c18c373820de7b821921d0013cd7
Marc Partensky <marc@marcpartensky.com>
2023-08-21 03:51:48 +0200

    seat group

--- configuration.nix
+++ configuration.nix
@@ -43,7 +43,7 @@ in
       isNormalUser = true;
       home = "/home/marc";
       description = "Marc Partensky";
-      extraGroups = ["wheel" "networkmanager" "docker"];
+      extraGroups = ["wheel" "networkmanager" "docker" "seat"];
       # openssh.authorizedKeys.keys = 
       shell = pkgs.zsh;
       initialHashedPassword = "$6$0QAYnBqAJtqB12p3$2lb7rAS2sYw49GUJt0L0bAEpZJSv4HZARQjlbYPhexSmeRB71IRMBzXjf3b4rX6fuDxOuDLydP/Kni9uraS5j/";

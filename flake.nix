98e44f40b239f71dd67eb7ad276fae3110cd59e1
Marc Partensky <marc@marcpartensky.com>
2023-08-21 04:34:49 +0200

    hyprland

--- flake.nix
+++ flake.nix
@@ -3,17 +3,28 @@
   ## after reboot, you can track rolling release by using
   #inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+     url = "github:nix-community/home-manager";
+     inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
   outputs = { self, nixpkgs }@inputs:
     let
       lib = nixpkgs.lib;
       mkHost = { my-config, zfs-root, pkgs, system, ... }:
         lib.nixosSystem {
           inherit system;
+          specialArgs = { inherit hyprland; };
           modules = [
             ./modules
             (import ./configuration.nix {
               inherit my-config zfs-root inputs pkgs lib;
             })
+            hyprland.nixosModules.default
           ];
         };
     in {

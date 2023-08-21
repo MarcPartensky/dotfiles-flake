f9db5b6a28090fc82b5ec3a30f23daf835809578
Marc Partensky <marc@marcpartensky.com>
2023-08-21 04:18:15 +0200

    gdm

--- configuration.nix
+++ configuration.nix
@@ -28,6 +28,7 @@ in
   networking.wireless.iwd.enable = true;
   networking.networkmanager.wifi.backend = "iwd";
 
+  systemd.services.gdm.enable = false;
   hardware.pulseaudio.enable = true;
   hardware.bluetooth.enable = true;
   programs.hyprland.enable = true;
@@ -169,7 +170,26 @@ in
       kubernetes
       killall
       seatd
-      ly
     ;
+  };
+
+  environment.sessionVariables = {
+    # POLKIT_AUTH_AGENT = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+    # GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
+    # LIBVA_DRIVER_NAME = "nvidia";
+    # XDG_SESSION_TYPE = "wayland";
+    # GBM_BACKEND = "nvidia-drm";
+    # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    # WLR_NO_HARDWARE_CURSORS = "1";
+    # NIXOS_OZONE_WL = "1";
+    # MOZ_ENABLE_WAYLAND = "1";
+    # SDL_VIDEODRIVER = "wayland";
+    # _JAVA_AWT_WM_NONREPARENTING = "1";
+    # CLUTTER_BACKEND = "wayland";
+    # WLR_RENDERER = "vulkan";
+    # XDG_CURRENT_DESKTOP = "Hyprland";
+    # XDG_SESSION_DESKTOP = "Hyprland";
+    # GTK_USE_PORTAL = "1";
+    # NIXOS_XDG_OPEN_USE_PORTAL = "1";
   };
 }
